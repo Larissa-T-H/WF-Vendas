7641f0879c41941ceeed10b00def91b31151bea6
Larissa-TH <larissatozetti@hotmail.com>
2022-04-28 11:46:27 -0300

    Vendas

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WindowsForms_Aula31
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProduto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.lstProduto = new System.Windows.Forms.ListBox();
+            this.lstPreco = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Produto";
+            // 
+            // txtProduto
+            // 
+            this.txtProduto.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProduto.Location = new System.Drawing.Point(74, 24);
+            this.txtProduto.Name = "txtProduto";
+            this.txtProduto.Size = new System.Drawing.Size(120, 28);
+            this.txtProduto.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(221, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Preço";
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPreco.Location = new System.Drawing.Point(267, 24);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(120, 28);
+            this.txtPreco.TabIndex = 1;
+            // 
+            // lstProduto
+            // 
+            this.lstProduto.FormattingEnabled = true;
+            this.lstProduto.Location = new System.Drawing.Point(16, 106);
+            this.lstProduto.Name = "lstProduto";
+            this.lstProduto.Size = new System.Drawing.Size(137, 225);
+            this.lstProduto.TabIndex = 2;
+            // 
+            // lstPreco
+            // 
+            this.lstPreco.FormattingEnabled = true;
+            this.lstPreco.Location = new System.Drawing.Point(184, 106);
+            this.lstPreco.Name = "lstPreco";
+            this.lstPreco.Size = new System.Drawing.Size(137, 225);
+            this.lstPreco.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(114, 366);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Total da Compra: R$";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(410, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 27);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Lançar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(136, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Remover";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 453);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstPreco);
+            this.Controls.Add(this.lstProduto);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtProduto);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtProduto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.ListBox lstProduto;
+        private System.Windows.Forms.ListBox lstPreco;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_Aula31
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                lstProduto.Items.Add(txtProduto.Text);
+                lstPreco.Items.Add(txtPreco.Text);
+           
+            txtProduto.Clear();
+            txtPreco.Clear();   
+
+            var somatoria = 0;
+
+            for(int i = 0; i < lstPreco.Items.Count; i++)
+            {
+                somatoria = somatoria+Convert.ToInt16(lstPreco.Items[i]);
+                   
+            }
+            label3.Text="Total da Compra: "+Convert.ToString(somatoria);
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            lstProduto.Items.RemoveAt(lstProduto.SelectedIndex);
+            lstPreco.Items.RemoveAt(lstPreco.SelectedIndex);
+
+            var somatoria = 0;
+            for (int i = 0; i < lstPreco.Items.Count; i++)
+            {
+                somatoria = somatoria+Convert.ToInt16(lstPreco.Items[i]);
+
+            }
+            label3.Text="Total da Compra: "+Convert.ToString(somatoria);
+
+        }
+    }
+}
